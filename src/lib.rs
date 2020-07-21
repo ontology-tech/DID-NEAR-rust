9faa4c05942a43ba793b1c6f4efba8fb6b16409f
siovanus <siovanus@126.com>
2020-07-21 19:01:14 +0800

    update

--- src/lib.rs
+++ src/lib.rs
@@ -34,10 +34,8 @@ impl DID {
         assert!(status.is_none());
 
         self.status.insert(&did, &Status::VALID);
-        self.public_key.insert(
-            &did,
-            &vec![PublicKey::new_pk_and_auth(&did, account_pk)],
-        );
+        self.public_key
+            .insert(&did, &vec![PublicKey::new_pk_and_auth(&did, account_pk)]);
         let index: u32 = 0;
         self.authentication.insert(&did, &vec![index]);
         self.created.insert(&did, &env::block_timestamp());
@@ -87,10 +85,7 @@ impl DID {
         self.controller.insert(&did, &controller_list);
         self.updated.insert(&did, &env::block_timestamp());
 
-        let log_message = format!(
-            "add_controller, id:{}, controller: {}",
-            &did, controller
-        );
+        let log_message = format!("add_controller, id:{}, controller: {}", &did, controller);
         env::log(log_message.as_bytes());
     }
 
@@ -112,10 +107,7 @@ impl DID {
         self.controller.insert(&did, &controller_list);
         self.updated.insert(&did, &env::block_timestamp());
 
-        let log_message = format!(
-            "remove_controller, id:{}, controller: {}",
-            &did, controller
-        );
+        let log_message = format!("remove_controller, id:{}, controller: {}", &did, controller);
         env::log(log_message.as_bytes());
     }
 
@@ -176,8 +168,7 @@ impl DID {
         let mut authentication_list = self.authentication.get(&did).unwrap();
         let index: u32 = (public_key_list.len() - 1) as u32;
         authentication_list.push(index);
-        self.authentication
-            .insert(&did, &authentication_list);
+        self.authentication.insert(&did, &authentication_list);
         self.updated.insert(&did, &env::block_timestamp());
 
         let log_message = format!(
@@ -200,8 +191,7 @@ impl DID {
         self.public_key.insert(&did, &public_key_list);
         let mut authentication_list = self.authentication.get(&did).unwrap();
         authentication_list.push(index as u32);
-        self.authentication
-            .insert(&did, &authentication_list);
+        self.authentication.insert(&did, &authentication_list);
         self.updated.insert(&did, &env::block_timestamp());
 
         let log_message = format!("set_auth_key, id:{}, public key: {:?}", &did, pk);
@@ -215,12 +205,10 @@ impl DID {
 
         self.check_did_status(&did);
         self.check_did_status(&controller_did);
-
         let controller_list = self.controller.get(&did).unwrap();
         if !controller_list.contains(&controller_did) {
             env::panic(b"add_new_auth_key_by_controller, signer is not controller")
         }
-
         let controller_public_key_list = self.public_key.get(&controller_did).unwrap();
         check_pk_access(&controller_public_key_list, &account_pk);
 
@@ -234,13 +222,41 @@ impl DID {
         let mut authentication_list = self.authentication.get(&did).unwrap();
         let index: u32 = (public_key_list.len() - 1) as u32;
         authentication_list.push(index);
-        self.authentication
-            .insert(&did, &authentication_list);
+        self.authentication.insert(&did, &authentication_list);
         self.updated.insert(&did, &env::block_timestamp());
 
         let log_message = format!(
             "add_new_auth_key_by_controller, id:{}, public key: {:?}, controller: {}",
             &did, pk, controller
+        );
+        env::log(log_message.as_bytes());
+    }
+
+    pub fn set_auth_key_by_controller(&mut self, did: String, pk: Vec<u8>) {
+        let account_id = env::signer_account_id();
+        let account_pk = env::signer_account_pk();
+        let controller_did = gen_did(&account_id);
+
+        self.check_did_status(&did);
+        self.check_did_status(&controller_did);
+        let controller_list = self.controller.get(&did).unwrap();
+        if !controller_list.contains(&controller_did) {
+            env::panic(b"set_auth_key_by_controller, signer is not controller")
+        }
+        let controller_public_key_list = self.public_key.get(&controller_did).unwrap();
+        check_pk_access(&controller_public_key_list, &account_pk);
+
+        let mut public_key_list = self.public_key.get(&did).unwrap();
+        let index = set_pk_auth(&mut public_key_list, &pk);
+        self.public_key.insert(&did, &public_key_list);
+        let mut authentication_list = self.authentication.get(&did).unwrap();
+        authentication_list.push(index as u32);
+        self.authentication.insert(&did, &authentication_list);
+        self.updated.insert(&did, &env::block_timestamp());
+
+        let log_message = format!(
+            "set_auth_key_by_controller, id:{}, public key: {:?}",
+            &did, pk
         );
         env::log(log_message.as_bytes());
     }
